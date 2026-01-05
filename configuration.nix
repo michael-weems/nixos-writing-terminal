4915e398c30a76015f4dd428e78a9d653491c10f
Michael Weems <mweems5@gmail.com>
2026-01-05 00:14:31 +0000

    disable firefox

--- configuration.nix
+++ configuration.nix
@@ -91,7 +91,7 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
-  programs.firefox.enable = true;
+  programs.firefox.enable = false;
   programs.waybar.enable = true;
   programs.hyprland = {
     enable = true;
